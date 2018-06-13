39b6b9d96b0e0ac678f08dcbf13075b260d0890e
Francisco <cbo317110@gmail.com>
2018-06-13 11:37:48 -0300

    fix local stylesheet namespace

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="hero container firestore-root">
+<div class="hero container app-fe-root">
   <h2 class="title is-2">
     Firestore Explorer
   </h2>
@@ -8,7 +8,7 @@
 </template>
 
 <style lang="scss">
-  .firestore-root {
+  .app-fe-root {
     padding-top: 1.5rem;
   }
 </style>
