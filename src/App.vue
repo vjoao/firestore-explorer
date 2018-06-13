1142c83f9391d554c5a087bbe2c88e165459dabc
Francisco <cbo317110@gmail.com>
2018-06-13 11:37:05 -0300

    padding without <br>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-<div class="hero container">
-  <br>
+<div class="hero container firestore-root">
   <h2 class="title is-2">
     Firestore Explorer
   </h2>
@@ -9,5 +8,7 @@
 </template>
 
 <style lang="scss">
-
+  .firestore-root {
+    padding-top: 1.5rem;
+  }
 </style>
